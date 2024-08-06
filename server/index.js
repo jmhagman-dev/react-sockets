03355b2ccfa7be20b4105f1ef4b53c9fdcfc9e27
Manny Hagman <jmhagman.dev@gmail.com>
2024-08-05 21:12:23 -0500

    Add server and client

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { createServer } = require('node:http');
+
+const app = express();
+const server = createServer(app);
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
