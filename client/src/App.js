a8b1af55805f60cad560c3b44aff86f3a9139df4
Manny Hagman <jmhagman.dev@gmail.com>
2024-08-05 22:58:02 -0500

    Add basic messaging

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+const socket = io.connect('http://localhost:4000');
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [inputData, setInputData] = useState('')
+  useEffect(() => {
+    // Connection
+    socket.on('connect', () => {
+      
+      console.log('+ Connected');
+    
+
+
+    });
+
+    // Disconnection
+    socket.on('disconnect', () => {
+      console.log('disconnecting')
+    });
+
+    // Turning off socket
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.on('messageResponse', (data) => setMessages([...messages, data]));
+  }, [messages]);
+
+
+  function handleClick() {
+    socket.emit('message', {
+      text: inputData,
+      id: `jmh`,
+      socketId: 'jmh',
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <pre>{JSON.stringify(messages, null, 2)}</pre>
+      <input type='text' onChange={(event) => setInputData(event.target.value)} />
+      <button onClick={handleClick}>Click</button>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,56 @@
 const express = require('express');
 const { createServer } = require('node:http');
-
+const { Server } = require('socket.io');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+});
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+const port = 4000;
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+
+io.on('connection', (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+
+  socket.on('message', (data) => {
+    console.log('sending message to client', data);
+    const status = ['building', 'placing', 'finishing', 'done'];
+    socket.emit('messageResponse', {
+      message: 'received build request'
+    })
+    let i = 0;
+    let interval = setInterval(function () {
+      
+      if (i === 3) {
+        clearInterval(interval);
+      }
+      socket.emit('messageResponse', {
+        status: status[i]
+      })
+      i++
+    }, 4000);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('🔥: A user disconnected');
+  });
+});
+
+app.get('/api', (req, res, next) => {
+  res.status(200).send({ message: 'welcome' });
+});
+
+server.listen(port, () => {
+  console.log('server running on ' + port);
 });
